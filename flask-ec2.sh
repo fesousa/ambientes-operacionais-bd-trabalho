ae701dfbc50d478d773b969812a33d852d584b91
fesousa <fernandosousa.ti@gmail.com>
2022-04-15 17:00:40 -0300

    Atualização

--- flask-ec2.sh
+++ flask-ec2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 yum update -y
-yum install python3-venv -y
-yum install git -y
+yum install python-pip python-dev nginx git -y
 cd /home/ec2-user
 su ec2-user -c "git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git"
 cd ambientes-operacionais-bd-trabalho
-python3 -m venv venv
-source venv/bin/activate
+pip install virtualenv
+virtualenv venv
+source ./venv/bin/activate
 pip install gunicorn flask
 cp app.service /etc/systemd/system/app.service
 systemctl daemon-reload
