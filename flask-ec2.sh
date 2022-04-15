14ed23c4cc856ce63946c1a30782df06049c3fd7
fesousa <fernandosousa.ti@gmail.com>
2022-04-15 17:33:37 -0300

    Atualização

--- flask-ec2.sh
+++ flask-ec2.sh
@@ -12,5 +12,10 @@ cd /var/www
 git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /var/www
 python3 -m venv venv
 source venv/bin/activate
-
-
+cp /var/www/config/app.service /etc/systemd/system
+chmod -R 755 /var/www
+chown -R ec2-user:nginx /var/www
+mkdir /var/log/uwsgi
+chown ec2-user:nginx /var/log/uwsgi/
+systemctl start flasktodo
+systemctl enable flasktodo
