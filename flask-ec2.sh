3029e3185e823080a8b7f2ea70e2cdbfd27bdcbd
fesousa <fernandosousa.ti@gmail.com>
2022-04-15 13:03:28 -0300

    Atualização

--- flask-ec2.sh
+++ flask-ec2.sh
@@ -4,3 +4,7 @@ yum install python3-venv -y
 yum install git -y
 cd /home/ec2-user
 su ec2-user -c "git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git"
+cd ambientes-operacionais-bd-trabalho
+python3 -m venv venv
+source venv/bin/activate
+pip install Flask
