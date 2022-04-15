1882890e982e4de6513082df3cfdb09951ab1872
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-15 20:07:56 -0300

    atualização

--- flask-ec2.sh
+++ flask-ec2.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 yum update -y
+
+pip3 install virtualenv
+mkdir /home/ec2-user/repo
+cd /home/ec2-user/repo
+git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /home/ec2-user/repo
+cd /home/ec2-user/repo
+
+virtualenv venv
+source venv/bin/activate
+
+pip3 install flask gunicorn
+
+cp /home/ec2-user/repo/app.service /etc/systemd/system/app.service
+
+amazon-linux-extras install nginx1 -y
+
+
 yum install python3-pip python-dev nginx -y
 yum install git -y
 pip3 install virtualenv
 
-mkdir /home/ec2-user/app
-cd /home/ec2-user/app
-git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /home/ec2-user/app
+mkdir /home/ec2-user/repo
+cd /home/ec2-user/repo
+git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /home/ec2-user/repo
+cd /home/ec2-user/repo/app
 
 virtualenv venv
 source ./venv/bin/activate
 pip3 install gunicorn flask
-cp app.conf /etc/init/myproject.conf
+mkdir /etc/init
+cp /home/ec2-user/repo/app.conf /etc/init/app.conf
