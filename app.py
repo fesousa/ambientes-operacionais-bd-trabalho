cbb65f1da53a061710a1f0f0ee9ebca6771a3012
fesousa <fernandosousa.ti@gmail.com>
2022-04-15 17:01:07 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+application = Flask(__name__)
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+@application.route("/")
+def index():
+    return "Hello World!"
 
 if __name__ == "__main__":
-	app.run()
+    application.run(host='0.0.0.0', port='8080')
