12311b711934d6b0bd1d3e2cc05a30cbf29ec512
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-15 20:34:44 -0300

    atualização

--- flask-ec2.sh
+++ flask-ec2.sh
@@ -9,7 +9,18 @@ yum install python3-pip python3-devel python3-setuptools -y
 mkdir /var/www
 cd /var/www
 git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /var/www
-
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+cp /var/www/app.service /etc/systemd/system
+chmod -R 755 /var/www
+chown -R ec2-user:nginx /var/www
+mkdir /var/log/uwsgi
+chown ec2-user:nginx /var/log/uwsgi/
+systemctl start app
+systemctl enable app
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf-orig
+cp nginx.conf /etc/nginx/nginx.conf
+cp app.conf /etc/nginx/conf.d/app.conf
+systemctl start nginx
+systemctl enable nginx
