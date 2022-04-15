e25cd19cd9bd9dd039eb07b291690a12349b70e4
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-15 20:16:25 -0300

    atualização

--- flask-ec2.sh
+++ flask-ec2.sh
@@ -1,33 +1,15 @@
 #!/bin/bash
 yum update -y
-
-pip3 install virtualenv
-mkdir /home/ec2-user/repo
-cd /home/ec2-user/repo
-git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /home/ec2-user/repo
-cd /home/ec2-user/repo
-
-virtualenv venv
-source venv/bin/activate
-
-pip3 install flask gunicorn
-
-cp /home/ec2-user/repo/app.service /etc/systemd/system/app.service
-
-amazon-linux-extras install nginx1 -y
-
-
-yum install python3-pip python-dev nginx -y
+amazon-linux-extras install epel -y
+yum install nginx -y
 yum install git -y
-pip3 install virtualenv
+yum install gcc -y
+yum install build-essential -y
+yum install python3-pip python3-devel python3-setuptools -y
+mkdir /var/www
+cd /var/www
+git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /var/www
 
-mkdir /home/ec2-user/repo
-cd /home/ec2-user/repo
-git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /home/ec2-user/repo
-cd /home/ec2-user/repo/app
-
-virtualenv venv
-source ./venv/bin/activate
-pip3 install gunicorn flask
-mkdir /etc/init
-cp /home/ec2-user/repo/app.conf /etc/init/app.conf
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
