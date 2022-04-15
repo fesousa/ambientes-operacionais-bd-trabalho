185fd525c83d821db05f85ce5a833de8eef5f079
fesousa <fernandosousa.ti@gmail.com>
2022-04-15 13:19:57 -0300

    Update flask-ec2.sh

--- flask-ec2.sh
+++ flask-ec2.sh
@@ -7,6 +7,8 @@ su ec2-user -c "git clone https://github.com/fesousa/ambientes-operacionais-bd-t
 cd ambientes-operacionais-bd-trabalho
 python3 -m venv venv
 source venv/bin/activate
-pip install Flask
-pip install gunicorn
-
+pip install gunicorn flask
+cp app.service /etc/systemd/system/app.service
+systemctl daemon-reload
+systemctl start app
+systemctl enable app
