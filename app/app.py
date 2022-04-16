189cdb03fe1106cbd362818b4057285e4e56792a
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-16 11:29:04 -0300

    atualização

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import yaml, traceback, os
+import psycopg2
 
 app = Flask(__name__)
 app.secret_key = 'AMBIENTESOPERACIONAIS'
@@ -13,7 +14,7 @@ def index():
                 config= yaml.load(file, Loader=yaml.FullLoader)
         except: 
             pass
-        return f"HOST: {config}"
+        return f"OK"
     except Exception as ex:
         return f"{traceback.format_exc()}, {os.getcwd()}"
 
@@ -37,9 +38,38 @@ def config():
         with open(f"{os.getcwd()}/app/config.yaml", "w") as file:
             yaml.dump(config, file)
       
-        return f"HOST: {config}"
+        return "OK"
     except Exception as ex:
         return traceback.format_exc()
+
+
+@app.route("/sql", methods = ['POST'])
+def sql():
+    try:
+        config = {}
+        try:
+            with open(f"{os.getcwd()}/app/config.yaml", "r") as file:
+        
+                config= yaml.load(file, Loader=yaml.FullLoader)
+        except:
+            raise Exception("Conexão não configurada")
+
+        con = psycopg2.connect(host=config["HOST"], database=config["DB"],
+        user=config["USER"], password=config["PWD"])
+        cur = con.cursor()
+        sql = request.json['sql']
+        r = cur.execute(sql)       
+        con.commit()
+        # cur.execute('select * from cidade')
+        # recset = cur.fetchall()
+        # for rec in recset:
+        # print (rec)
+        con.close()
+      
+        return r
+    except Exception as ex:
+        return traceback.format_exc()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
