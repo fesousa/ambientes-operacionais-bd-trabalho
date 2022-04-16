7eb9d74db7079327ff1f940b75eda74489d540f7
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-16 10:46:12 -0300

    atualização

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import dotenv, os, traceback
+import yaml, traceback
 
 app = Flask(__name__)
 app.secret_key = 'AMBIENTESOPERACIONAIS'
@@ -7,9 +7,12 @@ app.secret_key = 'AMBIENTESOPERACIONAIS'
 @app.route("/")
 def index():
     try:
-        dotenv_file = dotenv.find_dotenv()
-        dotenv.load_dotenv(dotenv_file)
-        return f"HOST: {os.environ}, {dotenv_file}"
+        config = None
+        with open("config.yaml", "r") as stream:
+    
+            config= yaml.safe_load(stream)
+ 
+        return f"HOST: {config}"
     except Exception as ex:
         return str(ex)
 
@@ -17,13 +20,10 @@ def index():
 @app.route("/config", methods = ['POST'])
 def config():
     try:
-        dotenv_file = dotenv.find_dotenv()
-        dotenv.load_dotenv(dotenv_file)
-        dotenv.set_key(dotenv_file, "HOST", "HOST DB")
-
-        return f"HOST: {os.environ}"
+      
+        return f"HOST: "
     except Exception as ex:
-        return dotenv_file
+        return traceback.format_exc()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
