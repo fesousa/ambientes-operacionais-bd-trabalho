854d9187a17706ca8f2ea9b9c140d80f79905740
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-16 10:10:49 -0300

    atualização

--- app/app.py
+++ app/app.py
@@ -1,17 +1,23 @@
-from crypt import methods
-from flask import Flask, session
+from flask import Flask
+import dotenv
+
 app = Flask(__name__)
 app.secret_key = 'AMBIENTESOPERACIONAIS'
 
 @app.route("/")
 def index():
-    session['username'] = 'Fernando'
-    return "Hello World!"
+    config = dotenv.dotenv_values(".env") 
+    return f"HOST: {config['HOST']}"
 
 
 @app.route("/config", methods = ['POST'])
 def config():
-    return f"Session: {session['username']}"
+    
+    dotenv.load_dotenv(".env")
+    dotenv.set_key(dotenv_file, "HOST", "HOST DB")
+
+    config = dotenv.dotenv_values(".env") 
+    return f"HOST: {config['HOST']}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
