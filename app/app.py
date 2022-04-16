6fa8d6f5bdc025505d4bd3841ad10ef8eb04fc9d
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-15 21:54:05 -0300

    atualização

--- app/app.py
+++ app/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, session
 app = Flask(__name__)
+app.secret_key = 'AMBIENTESOPERACIONAIS'
 
 @app.route("/")
 def index():
+    session['username'] = 'Fernando'
     return "Hello World!"
+
+
+@app.route("/config")
+def config():
+    return f"Session: {session['username']}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
