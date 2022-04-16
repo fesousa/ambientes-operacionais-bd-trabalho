43783b5d7c358f8a9cc2ec0cc66750f920c40321
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-16 11:02:43 -0300

    atualização

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import yaml, traceback, os
 
 app = Flask(__name__)
@@ -8,10 +8,8 @@ app.secret_key = 'AMBIENTESOPERACIONAIS'
 def index():
     try:
         config = None
-        with open(f"{os.getcwd()}/app/config.yaml", "r") as stream:
-    
-            config= yaml.safe_load(stream)
- 
+        with open(f"{os.getcwd()}/app/config.yaml", "r") as file:    
+            config= yaml.load(file, Loader=yaml.FullLoader)
         return f"HOST: {config}"
     except Exception as ex:
         return f"{traceback.format_exc()}, {os.getcwd()}"
@@ -20,8 +18,20 @@ def index():
 @app.route("/config", methods = ['POST'])
 def config():
     try:
+        config = None
+        with open(f"{os.getcwd()}/app/config.yaml", "r") as file:
+    
+            config= yaml.load(file, Loader=yaml.FullLoader)
+
+        config["HOST"] = request.json['host']
+        config["DB"] = request.json['bd']
+        config["USER"] = request.json['usuario']
+        config["PWD"] = request.json['senha']
+
+        with open(f"{os.getcwd()}/app/config.yaml", "w") as file:
+            yaml.dump(config, file)
       
-        return f"HOST: "
+        return f"HOST: {config}"
     except Exception as ex:
         return traceback.format_exc()
 
