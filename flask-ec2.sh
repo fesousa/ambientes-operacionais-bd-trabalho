8d3216515a479e434a0fcf4d7f4f5b676125514d
Fernando Sousa <fernandosousa.ti@gmail.com>
2022-04-16 12:55:24 -0300

    atualização

--- flask-ec2.sh
+++ flask-ec2.sh
@@ -5,7 +5,7 @@ yum install nginx -y
 yum install git -y
 yum install gcc -y
 yum install build-essential -y
-yum install python3-pip python3-devel python3-setuptools libpq-dev -y
+yum install python3-pip python3-devel python3-setuptools -y
 mkdir /var/www
 cd /var/www
 git clone https://github.com/fesousa/ambientes-operacionais-bd-trabalho.git /var/www
